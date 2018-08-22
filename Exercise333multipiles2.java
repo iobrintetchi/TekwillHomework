581011ff5cdd278066205ad09bb89d5bd228eed9
MacBook <macbook@MacBooks-MacBook-Air.local>
2018-08-22 15:28:27 +0300

    done ex multipliers of 2

---
+++ Exercise333multipiles2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise3.pkg33multipiles2;
+
+/**
+ *
+ * @author macbook
+ */
+public class Exercise333multipiles2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+     for (int b = 1;true;b++){
+     int mult = b*2;
+   System.out.println( mult );
+    }
+    
+}
+}
